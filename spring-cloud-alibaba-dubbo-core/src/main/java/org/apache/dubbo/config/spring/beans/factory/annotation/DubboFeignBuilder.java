92e056bc4345c2778d4c3423f8d180e997605786
dcy <1540770111@qq.com>
2020-02-09 16:34:15 +0800

    修改注册bug

--- spring-cloud-alibaba-dubbo-core/src/main/java/org/apache/dubbo/config/spring/beans/factory/annotation/DubboFeignBuilder.java
+++ spring-cloud-alibaba-dubbo-core/src/main/java/org/apache/dubbo/config/spring/beans/factory/annotation/DubboFeignBuilder.java
@@ -7,24 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.ReflectionUtils;
 
+import static org.apache.dubbo.config.spring.util.AnnotationUtils.getAttributes;
+import static org.springframework.core.annotation.AnnotationAttributes.fromMap;
+
 public class DubboFeignBuilder extends Feign.Builder {
     @Autowired
     private ApplicationContext applicationContext;
 
     public Reference defaultReference;
-    final class DefaultReferenceClass{
-        @Reference(check = false) String field;
+
+    final class DefaultReferenceClass {
+        @Reference(check = false)
+        String field;
     }
 
     public DubboFeignBuilder() {
-        this.defaultReference = ReflectionUtils.findField(DubboFeignBuilder.DefaultReferenceClass.class,"field").getAnnotation(Reference.class);
+        this.defaultReference = ReflectionUtils.findField(DubboFeignBuilder.DefaultReferenceClass.class, "field").getAnnotation(Reference.class);
     }
 
 
     @Override
     public <T> T target(Target<T> target) {
         ReferenceBeanBuilder beanBuilder = ReferenceBeanBuilder
-                .create(defaultReference, target.getClass().getClassLoader(), applicationContext)
+                .create(fromMap(getAttributes(defaultReference, applicationContext.getEnvironment(), true)), applicationContext)
                 .interfaceClass(target.type());
         try {
             T object = (T) beanBuilder.build().getObject();
